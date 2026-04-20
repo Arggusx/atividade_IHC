f84512f8798c5ab259fbce148463b7fcc4345520
Emylle Rassa <emyllerayssasilva@gmail.com>
2026-04-20 17:23:10 -0300

    adicionando estrutura do dashboard e shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginScreen from './pages/LoginScreen.tsx'
+import { DashboardShell } from './components/dashboardShell.tsx' // Importe o componente
 
 function App() {
 
@@ -8,6 +9,15 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginScreen />} />
+
+        <Route 
+          path="/dashboard" 
+          element={
+            <DashboardShell>
+            </DashboardShell>
+          } 
+        />
+
       </Routes>
     </BrowserRouter>
 
